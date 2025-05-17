c5c1cad0f4c82438eb7dbcfdfb1f36afc6031153
ALI120701 <alicb120701@gmail.com>
2025-05-17 10:22:50 +0500

    Initial commit: Flask backend with yt-dlp

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+
+app = Flask(__name__)
+
+@app.route('/api/video_info', methods=['POST'])
+def video_info():
+    data = request.json
+    url = data.get('url')
+    if not url:
+        return jsonify({'error': 'No URL provided'}), 400
+
+    ydl_opts = {'quiet': True, 'skip_download': True}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+
+    formats = [
+        {
+            'format_id': f['format_id'],
+            'ext': f['ext'],
+            'resolution': f.get('resolution'),
+            'filesize': f.get('filesize'),
+        }
+        for f in info['formats']
+    ]
+
+    return jsonify({
+        'title': info.get('title'),
+        'thumbnail': info.get('thumbnail'),
+        'formats': formats
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
